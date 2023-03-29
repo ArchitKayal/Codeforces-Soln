50292bda689f348996a0d2fd3f95e594dd05c326
ArchitKayal <59079120+ArchitKayal@users.noreply.github.com>
2023-03-29 23:29:04 +0530

    Create 479A-Expression.cpp

---
+++ 479A-Expression.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    vector<int> a(3);
+    for (int i = 0; i < 3; i++) {
+        cin >> a[i];
+    }
+ 
+    int mx = -1;
+    mx = max(mx, a[0] + a[1] + a[2]);
+    mx = max(mx, a[0] * (a[1] + a[2]));
+    mx = max(mx, (a[0] + a[1]) * a[2]);
+    mx = max(mx, a[0] * a[1] * a[2]);
+    cout << mx << '\n';
+}
